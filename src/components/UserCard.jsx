67f69e8d541b541a369868a69a1c12d06a4ebcea
seda <seda.diriker@gmail.com>
2024-07-15 21:52:49 +0300

    picture added

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,14 +3,14 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { getUser } from "../services/api";
 import SvgIcons from "./SvgIcons";
-import man from "../assets/man.svg"
-import mail from "../assets/mail.svg"
-import growingMan from "../assets/growing-up-man.svg"
-import growingWomen from "../assets/growing-up-woman.svg"
-import map from "../assets/map.svg"
-import phone from "../assets/phone.svg"
-import lock from "../assets/padlock.svg"
-import { Container } from "react-bootstrap";
+import man from "../assets/man.svg";
+import mail from "../assets/mail.svg";
+import growingMan from "../assets/growing-up-man.svg";
+import growingWomen from "../assets/growing-up-woman.svg";
+import map from "../assets/map.svg";
+import phone from "../assets/phone.svg";
+import lock from "../assets/padlock.svg";
+import { Container, Image } from "react-bootstrap";
 const UserCard = () => {
   const [user, setuser] = useState(null);
 
@@ -23,23 +23,26 @@ const UserCard = () => {
     fetchData();
   }, []);
 
-
   return (
-    <Card style={{ width: "57%", height: "60%", textAlign:"center" }}>
-      <Card.Header style={{ height: "30%" }}></Card.Header>
+    <Card style={{ width: "57%", height: "60%", textAlign: "center" }}>
+      <Card.Header className=" position-relative" style={{ height: "30%" }}>
+        <Image className=" position-absolute" src={user[0].picture.large} style={{outline:"1px solid gray", border:"5px solid white", top:"30%", left:"42%"}} roundedCircle />
+      </Card.Header>
       <Card.Body>
-        <Card.Title>
-          <p className="user-title" style={{color:"gray"}}>My name is</p>
+        <Card.Title className=" mt-5">
+          <p className="user-title" style={{ color: "gray" }}>
+            My name is
+          </p>
         </Card.Title>
         <Card.Text>{user && user[0].name.first}</Card.Text>
         <Container className="d-flex justify-content-center gap-5">
-        <SvgIcons icon={man}/>
-        <SvgIcons icon={mail}/>
-        <SvgIcons icon={growingMan}/>
-        <SvgIcons icon={map}/>
-        <SvgIcons icon={mail}/>
-        <SvgIcons icon={phone}/>
-        <SvgIcons icon={lock}/>
+          <SvgIcons icon={man} />
+          <SvgIcons icon={mail} />
+          <SvgIcons icon={growingMan} />
+          <SvgIcons icon={map} />
+          <SvgIcons icon={mail} />
+          <SvgIcons icon={phone} />
+          <SvgIcons icon={lock} />
         </Container>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
