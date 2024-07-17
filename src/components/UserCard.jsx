af7fa74d409a59d8572413015f4a8288bc8e376d
seda <seda.diriker@gmail.com>
2024-07-17 13:02:54 +0300

    icons statement added

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -15,20 +15,30 @@ import Btn from "./Btn";
 import AddUserTable from "./AddUserTable";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/Toastify";
 
-
 const UserCard = () => {
   const [user, setuser] = useState(false);
   const [selectedUsers, setSelectedUsers] = useState([]);
+  const [ıconTıtle, setIconTıtle] = useState("My name is");
+  const [text, setText] = useState(user[0]?.name?.first);
+
+  console.log(user);
 
   const icons = [
-    { icon: user[0]?.gender === "male" ? man : woman },
-    { icon: mail },
-    { icon: user[0]?.gender === "male" ? growingMan : growingWomen },
-    { icon: map },
-    { icon: phone },
-    { icon: lock },
+    {
+      icon: user[0]?.gender === "male" ? man : woman,
+      text: "My name is",
+      value: user[0]?.name?.first,
+    },
+    { icon: mail, text: "My email is", value: user[0]?.email },
+    {
+      icon: user[0]?.gender === "male" ? growingMan : growingWomen,
+      text: "My age is",
+      value: user[0]?.dob.age,
+    },
+    { icon: map, text: "My street is", value: user[0]?.location.street?.name },
+    { icon: phone, text: "My phone is", value:user[0]?.phone },
+    { icon: lock, text: "My password is", value:user[0]?.login.password },
   ];
-  
 
   const fetchData = async () => {
     const data = await getUser();
@@ -47,8 +57,6 @@ const UserCard = () => {
       toastSuccessNotify("Kullanıcı başarıyla eklendi.");
     }
   };
-
-  const mouseOverText = () => {};
 
   useEffect(() => {
     fetchData();
@@ -72,13 +80,22 @@ const UserCard = () => {
       <Card.Body>
         <Card.Title className=" mt-5">
           <p className="user-title" style={{ color: "gray" }}>
-            My name is
+            {ıconTıtle}
           </p>
         </Card.Title>
-        <Card.Text>{user && user[0].name.first}</Card.Text>
+        <Card.Text className=" fs-2">{text}</Card.Text>
         <Container className="d-flex justify-content-center gap-5">
           {icons.map((item, index) => (
-            <SvgIcons key={index} icon={item.icon} />
+            <div
+              key={index}
+              onMouseOver={() => {
+                setIconTıtle(item.text);
+                setText(item.value);
+              }}
+              style={{cursor:"pointer"}} 
+            >
+              <SvgIcons icon={item.icon} />
+            </div>
           ))}
         </Container>
         <Container className=" d-flex justify-content-center">
